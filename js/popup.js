b8059d14b851e5d86569d650e2d6ce53a30b2c81
Littlewolfwork <36741698+Littlewolfwork@users.noreply.github.com>
2020-06-16 23:54:37 +0500

    continue work

--- js/popup.js
+++ js/popup.js
@@ -6,7 +6,7 @@ $(function(){
             currentTasks = items.tasks;       
             if(currentTasks.length >0){
               for(var i in currentTasks){
-                  var add_html = '<li><b>'+currentTasks[i].date+' '+currentTasks[i].time+'</b> '+currentTasks[i].task+'</li>';
+                  var add_html = '<li><b>'+currentTasks[i].date+' '+currentTasks[i].time+'</b> '+currentTasks[i].task+' <img src="/image/del.png" alt="Удалить" class="task-delete"></li>';
                   $("ul").append(add_html);
               }
             }
