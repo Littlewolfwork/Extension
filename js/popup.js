744231c89070febc267d83eed28598a2028a91bb
Littlewolfwork <36741698+Littlewolfwork@users.noreply.github.com>
2020-06-18 16:03:33 +0500

    version 1.2.0.11

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,12 @@
 $(function(){
     var storage = chrome.storage.sync;
     storage.get("tasks",function(items){
-        console.log(items);
+       // console.log(items);
         if(items.tasks){
             currentTasks = items.tasks;       
             if(currentTasks.length >0){
               for(var i in currentTasks){
-                  var add_html = '<li><b>'+currentTasks[i].date+' '+currentTasks[i].time+'</b> '+currentTasks[i].task+' <img src="/image/del.png" alt="Удалить" class="task-delete"></li>';
+                  var add_html = '<li><b>'+currentTasks[i].date+' '+currentTasks[i].time+'</b> '+currentTasks[i].task+' <img src="/image/del.png" alt="Удалить" class="task-delete" id="task'+i+'"></li>';
                   $("ul").append(add_html);
               }
             }
@@ -14,6 +14,20 @@ $(function(){
   });
 });
 
+
+$('.task-delete').click(function(e){
+    alert('test');
+    console.log('test');
+  /*  let id = e.target.id;
+    console.log(id);*/
+
+
+});
+
 $('.clear-button').click(function(e){
     chrome.storage.sync.clear();
+    console.log('test');
 });
+
+
+
