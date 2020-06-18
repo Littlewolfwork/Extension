5b373e37c1ba3ab1801102164789acb6434cd8c6
Littlewolfwork <36741698+Littlewolfwork@users.noreply.github.com>
2020-06-18 16:47:00 +0500

    Delete one task

--- js/popup.js
+++ js/popup.js
@@ -6,7 +6,7 @@ $(function(){
             currentTasks = items.tasks;       
             if(currentTasks.length >0){
               for(var i in currentTasks){
-                  var add_html = '<li><b>'+currentTasks[i].date+' '+currentTasks[i].time+'</b> '+currentTasks[i].task+' <img src="/image/del.png" alt="Удалить" class="task-delete" id="task'+i+'"></li>';
+                  var add_html = '<li id="li_task'+i+'"><b>'+currentTasks[i].date+' '+currentTasks[i].time+'</b> '+currentTasks[i].task+' <img src="/image/del.png" alt="Удалить" class="task-delete" id="task'+i+'"></li>';
                   $("ul").append(add_html);
               }
             }
@@ -15,19 +15,28 @@ $(function(){
 });
 
 
-$('.task-delete').click(function(e){
-    alert('test');
-    console.log('test');
-  /*  let id = e.target.id;
-    console.log(id);*/
-
-
-});
-
 $('.clear-button').click(function(e){
     chrome.storage.sync.clear();
-    console.log('test');
 });
 
+$("ul").on("click", ".task-delete", function(e){ 
+  
+  let id = Number((e.target.id).slice(4));
+  console.log(id);
+  var storage = chrome.storage.sync;
+        storage.get("tasks",function(items){
+            if(items.tasks){
+                var tasks = items.tasks;
+            }
+            else{
+                exit();
+            }
 
+            tasks.splice(id, 1);
+            storage.set({
+                tasks:tasks
+            });
+            $('#li_task'+id).remove();
+        });
+});
 
