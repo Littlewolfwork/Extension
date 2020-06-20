3064f49bf2357bd847bbe685cb6c55ffe728090e
Littlewolfwork <36741698+Littlewolfwork@users.noreply.github.com>
2020-06-20 23:16:02 +0500

    begin editing task

--- js/popup.js
+++ js/popup.js
@@ -6,7 +6,9 @@ $(function(){
             currentTasks = items.tasks;       
             if(currentTasks.length >0){
               for(var i in currentTasks){
-                  var add_html = '<li id="li_task'+i+'"><b>'+currentTasks[i].date+' '+currentTasks[i].time+'</b> '+currentTasks[i].task+' <img src="/image/del.png" alt="Удалить" class="task-delete" id="task'+i+'"></li>';
+                  let add_html = '<li id="li_task'+i+'"><b>'+currentTasks[i].date+' '+currentTasks[i].time+'</b> '+currentTasks[i].task+
+                  '<img src="/image/edit.png" alt="Редактировать" class="task-edit" id="task_edit'+i+'">'+
+                  '<img src="/image/del.png" alt="Удалить" class="task-delete" id="task_del'+i+'"></li>';
                   $("ul").append(add_html);
               }
             }
@@ -15,13 +17,17 @@ $(function(){
 });
 
 
+function windowEditTask(){
+
+}
+
 $('.clear-button').click(function(e){
     chrome.storage.sync.clear();
 });
 
 $("ul").on("click", ".task-delete", function(e){ 
   
-  let id = Number((e.target.id).slice(4));
+  let id = Number((e.target.id).slice(8));
   console.log(id);
   var storage = chrome.storage.sync;
         storage.get("tasks",function(items){
@@ -40,3 +46,10 @@ $("ul").on("click", ".task-delete", function(e){
         });
 });
 
+$("ul").on("click", ".task-edit", function(e){ 
+  
+    let id = Number((e.target.id).slice(8));
+    console.log(id);
+    console.log('editing!!!');
+    $('#task-form-container').css("display","block");
+  });
