7959826351529e01938f1ecf6bbb8a8b79886f36
Littlewolfwork <36741698+Littlewolfwork@users.noreply.github.com>
2020-06-23 00:01:03 +0500

    Edit Task ready!

--- js/background.js
+++ js/background.js
@@ -2,17 +2,6 @@ chrome.contextMenus.create({
     'title': 'Добавить задачу', /* Текст пункта меню */
     'contexts':['selection'], /* Тип пункта меню */
     'onclick': function(info, tab) {  
-        
-        chrome.tabs.executeScript(
-            {
-                //Для подключения JavaScript файла нужно указать
-                //file : "content_script.js",
-                file : "js/popup_new_task.js",
-                code : "viewPopup("+info+")"
-            },
-            function() {
-                console.log("JavaScript executed!");
-            });
         var storage = chrome.storage.sync;
         storage.get("tasks",function(items){
             if(items.tasks){
@@ -26,6 +15,7 @@ chrome.contextMenus.create({
             var newTask = new Object();
             newTask.date = currentDate.getDate()+"."+(parseInt(currentDate.getMonth())+1)+"." + currentDate.getFullYear();
             newTask.time = checkTime(currentDate.getHours())+":"+ checkTime(currentDate.getMinutes());
+            newTask.responsible = getResponsibleTask(info.selectionText);
             newTask.task = info.selectionText;
             tasks[tasks.length] = newTask;
             storage.set({
@@ -36,6 +26,14 @@ chrome.contextMenus.create({
 
     } 
 });
+
+/*
+* Функция, которая парсит выделенную строку на предмет отвественного за задачу
+*/
+
+function getResponsibleTask(str){
+    return "";
+}
 
 function checkTime(i){
     if (i<10){
--- js/popup.js
+++ js/popup.js
@@ -6,7 +6,9 @@ $(function(){
             currentTasks = items.tasks;       
             if(currentTasks.length >0){
               for(var i in currentTasks){
-                  let add_html = '<li id="li_task'+i+'"><b>'+currentTasks[i].date+' '+currentTasks[i].time+'</b> '+currentTasks[i].task+
+                  let add_html = '<li id="li_task'+i+'"><b><span class="date">'+currentTasks[i].date+
+                  '</span> <span class="time">'+currentTasks[i].time+
+                  '</span></b> <span class="task">'+currentTasks[i].task+
                   '<img src="/image/edit.png" alt="Редактировать" class="task-edit" id="task_edit'+i+'">'+
                   '<img src="/image/del.png" alt="Удалить" class="task-delete" id="task_del'+i+'"></li>';
                   $("ul").append(add_html);
@@ -23,6 +25,7 @@ function windowEditTask(){
 
 $('.clear-button').click(function(e){
     chrome.storage.sync.clear();
+    $("ul").empty();
 });
 
 $("ul").on("click", ".task-delete", function(e){ 
@@ -48,9 +51,15 @@ $("ul").on("click", ".task-delete", function(e){
 
 $("ul").on("click", ".task-edit", function(e){ 
   
-    let id = Number((e.target.id).slice(8));
+    let id = Number((e.target.id).slice(9));
     console.log(id);
     console.log('editing!!!');
+    $("#idTask").val(id);
+    $("#date").val($("#li_task"+id+" .date").text());
+    $("#time").val($("#li_task"+id+" .time").text());
+    $("#responsible").val($("#li_task"+id+" .responsible").text());
+    $("#task").val($("#li_task"+id+" .task").text());
+
     $('#task-form-container').css("display","block");
   });
 
@@ -60,4 +69,29 @@ $('.button-cancel').click(function(e){
 
 $('.button-ok').click(function(e){
     $('#task-form-container').css("display","none");
+    var storage = chrome.storage.sync;
+    storage.get("tasks",function(items){
+
+        // console.log(items);
+        currentTasks = items.tasks;  
+        let idTask =  $("#idTask").val();
+        currentTasks[idTask].date = $("#date").val();
+        currentTasks[idTask].time = $("#time").val();
+        currentTasks[idTask].responsible = $("#responsible").val();
+        currentTasks[idTask].task = $("#task").val();
+        storage.set({
+            tasks:currentTasks
+        });
+        
+        const editLi = '<li id="li_task'+idTask+'"><b><span class="date">'+currentTasks[idTask].date+
+        '</span> <span class="time">'+currentTasks[idTask].time+
+        '</span></b> <span class="task">'+currentTasks[idTask].task+
+        '<img src="/image/edit.png" alt="Редактировать" class="task-edit" id="task_edit'+idTask+'">'+
+        '<img src="/image/del.png" alt="Удалить" class="task-delete" id="task_del'+idTask+'"></li>';
+
+        $('#li_task'+idTask).html(editLi);
+    });
+        
+  
+
 });
